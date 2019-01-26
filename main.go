d89cd36fb617b932639d7263d40fa1c21ce8928c
codythegreat <maxiecody@gmail.com>
2019-01-25 21:04:30 -0600

    adding more functionality to api

--- main.go
+++ main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -24,14 +25,27 @@ type Author struct {
 	Lastname  string `json:"lastname"`
 }
 
+// Init books var as a slice Book struct
+var books []Book
+
 // Get all books
 func getBooks(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
 }
 
 // Get single book
 func getBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // get params
+	// loop through the books, find the correct ID
+	for _, item := range books {
+		if item.ID == params["ID"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
 }
 
 // Create a new book
@@ -49,8 +63,12 @@ func main() {
 	// init the mux router
 	r := mux.NewRouter()
 
-	// route handlers and endpoints
+	// mock data
+	books = append(books, Book{ID: "1", Isbn: "1156454", Title: "Book 1", Author: &Author{Firstname: "John1", Lastname: "Doe1"}})
+	books = append(books, Book{ID: "2", Isbn: "2156454", Title: "Book 2", Author: &Author{Firstname: "John2", Lastname: "Doe2"}})
+	books = append(books, Book{ID: "3", Isbn: "3156454", Title: "Book 3", Author: &Author{Firstname: "John3", Lastname: "Doe3"}})
 
+	// route handlers and endpoints
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
 	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
 	r.HandleFunc("/api/books", createBook).Methods("POST")
