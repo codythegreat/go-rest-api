19e8ebf61dfb874f17a3cf4155ced85746e26fbc
codythegreat <maxiecody@gmail.com>
2019-01-26 02:39:12 +0000

    finished creating all response/request functions

--- main.go
+++ main.go
@@ -3,20 +3,45 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
 // Book struct model
 
 type Book struct {
-	ID string `json:"id"`
-	Isbn string `json:"isbn"`
-	Title string `json:"title"`
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
 	Author *Author `json:"author"`
 }
 
 // Author struct
 type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+// Get all books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Get single book
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Create a new book
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+func deleteBook(w http.ResponseWriter, r *http.Request) {
 
 }
 
@@ -26,12 +51,12 @@ func main() {
 
 	// route handlers and endpoints
 
-	r.HandleFunc("/api/books", getBooks).Medthods("GET")
-	r.HandleFunc("/api/books/{id}", getBook).Medthods("GET")
-	r.HandleFunc("/api/books", createBook).Medthods("POST")
-	r.HandleFunc("/api/books/{id}", updateBook).Medthods("PUT")
-	r.HandleFunc("/api/books/{id}", deleteBook).Medthods("DELETE")
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 
 	// start the server
-	log.Fatal(http.ListenAndServer(":8000", r)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
