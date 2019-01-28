775c9e2e4367f4079e182b38261d99217bf0a1e9
codythegreat <maxiecody@gmail.com>
2019-01-28 15:44:55 +0000

    removed main.go and renamed main2 to main

--- main.go
+++ main.go
@@ -1,87 +1,88 @@
-// rest api server implementation in golang
-
 package main
 
 import (
 	"encoding/json"
-	"log"
-	"math/rand"
-	"net/http"
-	"strconv"
-
+	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 )
 
-// Book struct model
-
-type Book struct {
-	ID     string  `json:"id"`
-	Isbn   string  `json:"isbn"`
-	Title  string  `json:"title"`
-	Author *Author `json:"author"`
+type Artist struct {
+	Name  string `json:"Name"`
+	Genre string `json:"Genre"`
+	ID    string `json:"id"`
 }
 
-// Author struct
-type Author struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
-}
+var jsonArtists []Artist
 
-// Init books var as a slice Book struct
-var books []Book
-
-// Get all books
-func getBooks(w http.ResponseWriter, r *http.Request) {
+func getArtists(w http.ResponseWriter, r *http.Request) {
+	// set the content type to json format
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(books)
+	// encode all data to page
+	json.NewEncoder(w).Encode(jsonArtists)
 }
 
-// Get single book
-func getBook(w http.ResponseWriter, r *http.Request) {
+func getArtist(w http.ResponseWriter, r *http.Request) {
+	// set the content type to json format
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) // get params
-	// loop through the books, find the correct ID
-	for _, item := range books {
-		if item.ID == params["ID"] {
+	// extract the parameters of the search
+	params := mux.Vars(r)
+	for _, item := range jsonArtists {
+		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
 			return
 		}
 	}
-	json.NewEncoder(w).Encode(&Book{})
 }
 
-// Create a new book
-func createBook(w http.ResponseWriter, r *http.Request) {
+// for a given artist, retieve all instances
+func getArtistGenres(w http.ResponseWriter, r *http.Request) {
+	// set the content type to json format
 	w.Header().Set("Content-Type", "application/json")
-	var book Book
-	_ = json.NewDecoder(r.Body).Decode(&book)
-	book.ID = strconv.Itoa(rand.Intn(10000000)) // mock id
-	books = append(books, book)
-	json.NewEncoder(w).Encode(book)
+	// extract the parameters of the search
+	params := mux.Vars(r)
+	var items = []Artist{}
+	for _, item := range jsonArtists {
+		if item.Name == strings.Join(strings.Split(params["name"], "-"), " ") {
+			items = append(items, item)
+		}
+	}
+	json.NewEncoder(w).Encode(items)
 }
-func updateBook(w http.ResponseWriter, r *http.Request) {
 
-}
-func deleteBook(w http.ResponseWriter, r *http.Request) {
-
+// for a given genre, retrieve all artist in that genre
+func getGenres(w http.ResponseWriter, r *http.Request) {
+	// set the content type to json format
+	w.Header().Set("Content-Type", "application/json")
+	// extract the parameters of the search
+	params := mux.Vars(r)
+	var items = []Artist{}
+	for _, item := range jsonArtists {
+		if item.Genre == strings.Join(strings.Split(params["genre"], "-"), " ") {
+			items = append(items, item)
+		}
+	}
+	json.NewEncoder(w).Encode(items)
 }
 
 func main() {
-	// init the mux router
+	// read the json data from local file
+	byteValue, _ := ioutil.ReadFile("artistInfo.json")
+	// place the data in jsonArtists
+	err := json.Unmarshal(byteValue, &jsonArtists)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// initialize the mux router
 	r := mux.NewRouter()
-
-	// mock data
-	books = append(books, Book{ID: "1", Isbn: "1156454", Title: "Book 1", Author: &Author{Firstname: "John1", Lastname: "Doe1"}})
-	books = append(books, Book{ID: "2", Isbn: "2156454", Title: "Book 2", Author: &Author{Firstname: "John2", Lastname: "Doe2"}})
-	books = append(books, Book{ID: "3", Isbn: "3156454", Title: "Book 3", Author: &Author{Firstname: "John3", Lastname: "Doe3"}})
-
-	// route handlers and endpoints
-	r.HandleFunc("/api/books", getBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-	r.HandleFunc("/api/books", createBook).Methods("POST")
-	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
-	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
-
-	// start the server
-	log.Fatal(http.ListenAndServe(":8000", r))
+	// designate functions to handle routes
+	r.HandleFunc("/api/artists/all", getArtists).Methods("GET")
+	r.HandleFunc("/api/artists/ID={id}", getArtist).Methods("GET")
+	r.HandleFunc("/api/artists/NAME={name}", getArtistGenres).Methods("GET")
+	r.HandleFunc("/api/artists/GENRE={genre}", getGenres).Methods("GET")
+	// listen and respond to requests on port 8000
+	log.Fatal(http.ListenAndServe("45.76.248.143:8000", r))
 }
