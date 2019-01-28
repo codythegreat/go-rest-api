25c04655093db508a5041f2cdcaf08342b3971eb
codythegreat <maxiecody@gmail.com>
2019-01-28 15:42:09 +0000

    added more functionality to server

--- main2.go
+++ main2.go
@@ -3,12 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"io/ioutil"
 	"log"
 	"net/http"
-	//	"os"
-
-	"github.com/gorilla/mux"
+	"strings"
 )
 
 type Artist struct {
@@ -39,6 +38,36 @@ func getArtist(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// for a given artist, retieve all instances
+func getArtistGenres(w http.ResponseWriter, r *http.Request) {
+	// set the content type to json format
+	w.Header().Set("Content-Type", "application/json")
+	// extract the parameters of the search
+	params := mux.Vars(r)
+	var items = []Artist{}
+	for _, item := range jsonArtists {
+		if item.Name == strings.Join(strings.Split(params["name"], "-"), " ") {
+			items = append(items, item)
+		}
+	}
+	json.NewEncoder(w).Encode(items)
+}
+
+// for a given genre, retrieve all artist in that genre
+func getGenres(w http.ResponseWriter, r *http.Request) {
+	// set the content type to json format
+	w.Header().Set("Content-Type", "application/json")
+	// extract the parameters of the search
+	params := mux.Vars(r)
+	var items = []Artist{}
+	for _, item := range jsonArtists {
+		if item.Genre == strings.Join(strings.Split(params["genre"], "-"), " ") {
+			items = append(items, item)
+		}
+	}
+	json.NewEncoder(w).Encode(items)
+}
+
 func main() {
 	// read the json data from local file
 	byteValue, _ := ioutil.ReadFile("artistInfo.json")
@@ -50,8 +79,10 @@ func main() {
 	// initialize the mux router
 	r := mux.NewRouter()
 	// designate functions to handle routes
-	r.HandleFunc("/api/artists", getArtists).Methods("GET")
-	r.HandleFunc("/api/artists/{id}", getArtist).Methods("GET")
+	r.HandleFunc("/api/artists/all", getArtists).Methods("GET")
+	r.HandleFunc("/api/artists/ID={id}", getArtist).Methods("GET")
+	r.HandleFunc("/api/artists/NAME={name}", getArtistGenres).Methods("GET")
+	r.HandleFunc("/api/artists/GENRE={genre}", getGenres).Methods("GET")
 	// listen and respond to requests on port 8000
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe("45.76.248.143:8000", r))
 }
